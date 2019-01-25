9f0eb717cc55726ec04827efc25cc1bfbce29583
Vladislav Markov <vladislav.markov@esrlabs.com>
2019-01-25 18:44:48 +0100

    SDL demo

---
+++ src/no-warn-imgui.hpp
@@ -0,0 +1,10 @@
+#ifndef NO_WARN_IMGUI_HPP
+#define NO_WARN_IMGUI_HPP
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#include "imgui.h"
+#pragma GCC diagnostic pop
+
+#endif // NO_WARN_IMGUI_HPP
